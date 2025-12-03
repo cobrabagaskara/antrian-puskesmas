cab809ea4da218b64c5439ed764de321b6a7d5e4
cobrabagaskara <60558256+cobrabagaskara@users.noreply.github.com>
2025-12-03 16:41:59 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASLzAtjOgYI2Otko1vMD2Lie8_h3GCUsc",
+  authDomain: "antrian-puskesmas-5869a.firebaseapp.com",
+  databaseURL: "https://antrian-puskesmas-5869a-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "antrian-puskesmas-5869a",
+  storageBucket: "antrian-puskesmas-5869a.firebasestorage.app",
+  messagingSenderId: "992002959684",
+  appId: "1:992002959684:web:ec82ecb075c71927504f11"
+};
